510412bb7a771521abdc77cc49b8bdc7ebbc2ad1
hborcher <hborcher@illinois.edu>
2017-08-18 10:24:29 -0500

    moved images to _static folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,7 +114,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = 'full_mark_horz_bw.gif'
+html_logo = '_static/full_mark_horz_bw.gif'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
