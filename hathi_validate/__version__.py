2e324d1b272e2e920df16f53adf2605fbdd24c7e
hborcher <hborcher@illinois.edu>
2018-01-09 15:56:13 -0600

    upped version

--- hathi_validate/__version__.py
+++ hathi_validate/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'HathiValidate'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __url__ = 'https://github.com/UIUCLibrary/HathiValidate'
 __author__ = 'University of Illinois at Urbana Champaign'
 __author_email__ = 'hborcher@illinois.edu'
