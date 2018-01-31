b69d74ddc4742c8f3a62cf3a47fc7cc4116c0c69
hborcher <hborcher@illinois.edu>
2018-01-31 13:25:16 -0600

    import only version information dynamically

--- hathi_validate/__init__.py
+++ hathi_validate/__init__.py
@@ -1,2 +1,42 @@
-from .__version__ import  __version__, __author__, __author_email__, __description__, __title__, __url__, FULL_TITLE
+import os
+import sys
+
+from setuptools.config import read_configuration
+
 from .xml_schemes import get_scheme
+from pkg_resources import get_distribution, DistributionNotFound
+
+FULL_TITLE = "DS Hathi Trust Validate"
+
+
+def get_project_metadata(config_file):
+    return read_configuration(config_file)["metadata"]
+
+
+def get_version():
+    try:
+        package_metadata = get_distribution("HathiValidate")
+        version = package_metadata.version
+    except DistributionNotFound as e:
+
+        # =====================================================================
+        # In the case of CX_FREEZE. As of version 5.1.1, it doesn't build
+        # package metadata files. For this reason setup.cfg must be manually
+        # included as part of the setup script, which it includes it to the
+        # same path as the main exe.
+        # =====================================================================
+        setup_cfg = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../", "setup.cfg"))
+        if os.path.exists(setup_cfg):
+            metadata = get_project_metadata(setup_cfg)
+            if metadata["name"] == "HathiValidate":
+                return metadata["version"]
+        # =====================================================================
+
+        print("Has the metadata for this project been built?", file=sys.stderr)
+        version = "Unknown"
+    except FileNotFoundError as e:
+        version = "Unknown"
+    return version
+
+
+__version__ = get_version()
