9609add7bcaec3cbc4bf62a2d3cc1fe2b4061177
hborcher <hborcher@illinois.edu>
2018-01-31 14:07:06 -0600

    PSR-92 HathiTrust Package Completeness - BB Feedback

--- hathi_validate/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = 'HathiValidate'
-__version__ = '0.3.0'
-__url__ = 'https://github.com/UIUCLibrary/HathiValidate'
-__author__ = 'University of Illinois at Urbana Champaign'
-__author_email__ = 'hborcher@illinois.edu'
-__description__ = 'Replacement for older scripts for validating Hathi Trust Packages'
-FULL_TITLE = "DS Hathi Trust Validate"
