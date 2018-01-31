b98b511b02125dce0350495214251e75027c67b8
hborcher <hborcher@illinois.edu>
2018-01-31 13:24:55 -0600

    removed gui flag

--- hathi_validate/__main__.py
+++ hathi_validate/__main__.py
@@ -7,11 +7,11 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
         import pytest  # type: ignore
         sys.exit(pytest.main(sys.argv[2:]))
-    elif len(sys.argv) > 1 and "--gui" in sys.argv:
-        try:
-            hathi_validate.gui.main()
-        except ImportError:
-            print("Feature not currently installed", file=sys.stderr)
+    # elif len(sys.argv) > 1 and "--gui" in sys.argv:
+    #     try:
+    #         hathi_validate.gui.main()
+    #     except ImportError:
+    #         print("Feature not currently installed", file=sys.stderr)
     else:
         hathi_validate.cli.main()
 
