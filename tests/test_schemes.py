1ce9ea32b1ca1944075dfeb2116c40b1769f728e
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-07-26 16:56:10 -0500

    convert to string before comparing

--- tests/test_schemes.py
+++ tests/test_schemes.py
@@ -5,7 +5,8 @@ import hathi_validate
 def test_get_alto():
     alto = hathi_validate.get_scheme("alto")
     assert isinstance(alto, bytes)
-    assert alto.startswith(b'<?xml version="1.0" encoding="UTF-8"?>\n<!-- ALTO: Analyzed Layout and Text Object  -->')
+    lines = alto.decode().split("\n")
+    lines[0].startswith('<?xml version="1.0" encoding="UTF-8"?>\n<!-- ALTO: Analyzed Layout and Text Object  -->')
 
 
 def test_get_bad_scheme():
