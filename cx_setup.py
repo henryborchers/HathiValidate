470ae4fca5f02bcb9b90f73955569cd7480b14a0
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-07-27 10:33:43 -0500

    added html.parser to packages

--- cx_setup.py
+++ cx_setup.py
@@ -71,7 +71,7 @@ if os.path.exists(MSVC):
 build_exe_options = {
     "includes": ["queue", "atexit", "appdirs", 'pkg_resources'] + pytest.freeze_includes(),
     "include_msvcr": True,
-    "packages": ["os", "lxml", "packaging", "six", "appdirs", "hathi_validate", "setuptools"],
+    "packages": ["os", "lxml", "packaging", "six", "appdirs", "hathi_validate", "setuptools", "html"],
     "excludes": ["tkinter"],
     "include_files": INCLUDE_FILES,
 
